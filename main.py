49ba5566c04cb4bacd4d221bc37fbf36c067273f
Dhanasekar A K <126952078+dhanasekar-30@users.noreply.github.com>
2023-06-20 17:35:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
-from datetime import datetime
-from config import *
-from snowflake_connection import *
-from snowflake_unloader import *
+from file_compare import *
+
 
 if __name__ == '__main__':
-
-    try:
-        LOGGER.info("***************Starting Snowflake Connecting*******************")
-
-        #Snowflake Sync
-        loader = Snowflake_Unloader()
-        loader.SnowflakeAccess()
-        loader.unload()  
-        
-        LOGGER.info("------------Snowflake Connected Successfully----------------")
-        
-    except Exception as e:
-        LOGGER.error(str(e), exc_info=True)
-        LOGGER.info("Error Executing Snowflake Unloading")
+    checker = Compare()
+    checker.csv_compare()
